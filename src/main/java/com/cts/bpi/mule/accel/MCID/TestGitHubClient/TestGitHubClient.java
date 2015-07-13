033ed087b2897277c6c98eb599e12d90631f95a9
Girish <girishreddy.b@gmail.com>
2015-07-13 09:45:00 -0400

    Ignore this Client Code
    Ignore this Client Code

---
+++ src/main/java/com/cts/bpi/mule/accel/MCID/TestGitHubClient/TestGitHubClient.java
@@ -0,0 +1,51 @@
+package com.cts.bpi.mule.accel.MCID.TestGitHubClient;
+/*
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.kohsuke.github.GHBranch;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;*/
+
+public class TestGitHubClient {
+
+	/* Ignore the class public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GitHub github;
+		//Git git = Git.open(new File(""));
+		try {
+			
+			https://github.com/GirishReddyB/MySpring.git
+			github = GitHub.connectUsingPassword("girishreddyB", "radhakrishna@123");
+			//github.connectUsingPassword("girishreddyn", "radhakrishna@123");
+			System.out.println("github:"+github.getMyOrganizations());
+			
+			GHRepository repo = github.getRepository("GirishReddyB/MySpring");
+			System.out.println("Count : "+repo.getSubscribersCount());
+			System.out.println("Branches : "+repo.getBranches());
+			
+			Map<String, GHBranch> branchMap=repo.getBranches();
+		Set<String> branchList=	branchMap.keySet();
+		
+		for (Iterator iterator = branchList.iterator(); iterator.hasNext();) {
+			String object = (String) iterator.next();
+			GHBranch branch=branchMap.get(object);
+			System.out.println("Branch :: "+branch.getRoot());
+			
+		}
+			GHRepository fork=repo.fork();
+			//fork.getFileContent(path, ref);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}*/
+
+}
